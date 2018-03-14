a3cfd0767a8d90c5bb2785ccbb43004343991ffa
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-14 13:49:29 -0300

    Refactor tests
    Remove `spawn_server` functions, tweak timings, and refactor a test so
    that it's not time dependent, because the request doesn't need to be
    slow in that case.

--- http/tests/common/mod.rs
+++ http/tests/common/mod.rs
@@ -57,12 +57,3 @@ impl MockRpcServerApi for MockRpcServer {
         Ok(())
     }
 }
-
-#[allow(unused_macros)]
-macro_rules! assert_err {
-    ($error_chain1:expr, $error_chain2:expr) => {
-        for (error1, error2) in $error_chain1.iter().zip($error_chain2.iter()) {
-            assert_eq!(format!("{}", error1), format!("{}", error2));
-        }
-    }
-}
