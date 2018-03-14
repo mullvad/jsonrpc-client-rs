1b603b9a2dfc2d5e6cd08590b587a41973a03827
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-14 13:57:30 -0300

    Remove `Server`'s internal delay

--- http/tests/common/test_server.rs
+++ http/tests/common/test_server.rs
@@ -9,6 +9,9 @@ build_rpc_trait! {
         #[rpc(name = "to_upper")]
         fn to_upper(&self, String) -> Result<String, Error>;
 
+        #[rpc(name = "slow_to_upper")]
+        fn slow_to_upper(&self, String, u64) -> Result<String, Error>;
+
         #[rpc(name = "sleep")]
         fn sleep(&self, u64) -> Result<(), Error>;
     }
@@ -17,26 +20,15 @@ build_rpc_trait! {
 // Generate client struct with same API as server.
 jsonrpc_client!(pub struct TestClient {
     pub fn to_upper(&mut self, string: &str) -> RpcRequest<String>;
+    pub fn slow_to_upper(&mut self, string: &str, time: u64) -> RpcRequest<String>;
     pub fn sleep(&mut self, time: u64) -> RpcRequest<()>;
 });
 
 
 /// Simple struct that will implement the RPC API defined at the top of this file.
-///
-/// Can be configured to delay its responses to simulate long operations.
-pub struct Server {
-    delay: Option<Duration>,
-}
+pub struct Server;
 
 impl Server {
-    pub fn new() -> Self {
-        Server { delay: None }
-    }
-
-    pub fn with_delay(delay: Duration) -> Self {
-        Server { delay: Some(delay) }
-    }
-
     pub fn spawn(self) -> jsonrpc_http_server::Server {
         let mut io = IoHandler::new();
         io.extend_with(self.to_delegate());
@@ -45,24 +37,21 @@ impl Server {
             .start_http(&"127.0.0.1:0".parse().unwrap())
             .expect("failed to spawn server")
     }
-
-    fn simulate_delay(&self) {
-        if let Some(delay) = self.delay {
-            ::std::thread::sleep(delay);
-        }
-    }
 }
 
 impl ServerApi for Server {
     fn to_upper(&self, s: String) -> Result<String, Error> {
-        self.simulate_delay();
+        Ok(s.to_uppercase())
+    }
+
+    fn slow_to_upper(&self, s: String, time: u64) -> Result<String, Error> {
+        ::std::thread::sleep(Duration::from_secs(time));
         Ok(s.to_uppercase())
     }
 
     fn sleep(&self, time: u64) -> Result<(), Error> {
         println!("Sleeping on server");
         ::std::thread::sleep(Duration::from_secs(time));
-        self.simulate_delay();
         Ok(())
     }
 }
