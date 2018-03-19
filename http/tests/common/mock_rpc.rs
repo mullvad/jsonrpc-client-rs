807cbfcced817c12d41e8b44eee1d0485f493a91
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-19 11:45:25 -0300

    Rename `test_server` module and its types
    Make them have less ambiguous names that better convey their purpose.

--- http/tests/common/mock_rpc.rs
+++ http/tests/common/mock_rpc.rs
@@ -5,7 +5,7 @@ use jsonrpc_http_server::{self, ServerBuilder};
 
 // Generate server API trait. Actual implementation at bottom of file.
 build_rpc_trait! {
-    pub trait ServerApi {
+    pub trait MockRpcServerApi {
         #[rpc(name = "to_upper")]
         fn to_upper(&self, String) -> Result<String, Error>;
 
@@ -18,7 +18,7 @@ build_rpc_trait! {
 }
 
 // Generate client struct with same API as server.
-jsonrpc_client!(pub struct TestClient {
+jsonrpc_client!(pub struct MockRpcClient {
     pub fn to_upper(&mut self, string: &str) -> RpcRequest<String>;
     pub fn slow_to_upper(&mut self, string: &str, time: u64) -> RpcRequest<String>;
     pub fn sleep(&mut self, time: u64) -> RpcRequest<()>;
@@ -26,9 +26,9 @@ jsonrpc_client!(pub struct TestClient {
 
 
 /// Simple struct that will implement the RPC API defined at the top of this file.
-pub struct Server;
+pub struct MockRpcServer;
 
-impl Server {
+impl MockRpcServer {
     pub fn spawn(self) -> jsonrpc_http_server::Server {
         let mut io = IoHandler::new();
         io.extend_with(self.to_delegate());
@@ -39,7 +39,7 @@ impl Server {
     }
 }
 
-impl ServerApi for Server {
+impl MockRpcServerApi for MockRpcServer {
     fn to_upper(&self, s: String) -> Result<String, Error> {
         Ok(s.to_uppercase())
     }
