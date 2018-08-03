fdd6c3e45d3192a1385a8d65ceb172e38c4c1afc
Emīls Piņķis <emils@mullvad.net>
2018-08-03 12:58:38 +0100

    Remove old stuff from core

--- core/src/example.rs
+++ core/src/example.rs
@@ -5,21 +5,3 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-
-jsonrpc_client!(
-    /// Just an example RPC client to showcase how to use the `jsonrpc_client` macro and what
-    /// the resulting structs look like.
-    pub struct ExampleRpcClient {
-        /// A method without any arguments and with no return value. Can still of course have
-        /// lots of side effects on the server where it executes.
-        pub fn nullary(&mut self) -> RpcRequest<()>;
-
-        /// Send a string to the server and it will presumably echo it back.
-        pub fn echo(&mut self, input: &str) -> RpcRequest<String>;
-
-        /// Example RPC method named "concat" that takes a `String` and an unsigned integer and
-        /// returns a `String`. From the name one could guess it will concatenate the two
-        /// arguments. But that of course depends on the server where this call is sent.
-        pub fn concat(&mut self, arg0: String, arg1: u64) -> RpcRequest<String>;
-    }
-);
--- core/src/macros.rs
+++ core/src/macros.rs
@@ -6,43 +6,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-/// The main macro of this crate. Generates JSON-RPC 2.0 client structs with automatic serialization
-/// and deserialization. Method calls get correct types automatically.
-#[macro_export]
-macro_rules! jsonrpc_client {
-    (
-        $(#[$struct_attr:meta])*
-        pub struct $struct_name:ident {$(
-            $(#[$attr:meta])*
-            pub fn $method:ident(&mut $selff:ident $(, $arg_name:ident: $arg_ty:ty)*)
-                -> RpcRequest<$return_ty:ty>;
-        )*}
-    ) => (
-        $(#[$struct_attr])*
-        pub struct $struct_name<T: $crate::Transport> {
-            transport: T,
-        }
-
-        impl<T: $crate::Transport> $struct_name<T> {
-            /// Creates a new RPC client backed by the given transport implementation.
-            pub fn new(transport: T) -> Self {
-                $struct_name { transport }
-            }
-
-            $(
-                $(#[$attr])*
-                pub fn $method(&mut $selff $(, $arg_name: $arg_ty)*)
-                    -> $crate::RpcRequest<$return_ty, T::Future>
-                {
-                    let method = String::from(stringify!($method));
-                    let params = expand_params!($($arg_name,)*);
-                    $crate::call_method(&mut $selff.transport, method, params)
-                }
-            )*
-        }
-    )
-}
-
 /// Expands a variable list of parameters into its serializable form. Is needed to make the params
 /// of a nullary method equal to `[]` instead of `()` and thus make sure it serializes to `[]`
 /// instead of `null`.
